33ec1150125b5057181196e58f8571f866b88b0b
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    msm8937-common: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- msm8937.mk
+++ msm8937.mk
@@ -26,7 +26,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := framework-res
+PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += device/motorola/msm8937-common
