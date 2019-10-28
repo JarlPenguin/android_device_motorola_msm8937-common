3b3632db9229eb30644147b931d414765553c882
Erfan Abdi <erfangplus@gmail.com>
2019-10-28 12:23:46 +0530

    msm8937-common: Build AOSP camera provider service

--- msm8937.mk
+++ msm8937.mk
@@ -81,6 +81,7 @@ PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     libjustshoot_shim \
+    android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.4-impl \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
