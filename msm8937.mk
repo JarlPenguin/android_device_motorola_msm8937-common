78ecc797408058c07e11995a3b9acd9324be132c
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-01 07:19:07 -0500

    msm8937-common: Exclude lineage-sdk overlays from RRO
    * lineage-sdk overlays don't work with RRO
    
    Change-Id: Iff826faf1864388066a488acc9682636b98d1098

--- msm8937.mk
+++ msm8937.mk
@@ -27,6 +27,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += device/motorola/msm8937-common
