5bad28c214605e30dfc6ad92b5351795f5d6c978
Henrique Pereira <hlcpereira@pixelexperience.org>
2019-11-10 16:06:54 -0200

    msm8937-common: Switch to cryptfshw QSEECom impl

--- msm8937.mk
+++ msm8937.mk
@@ -134,7 +134,7 @@ PRODUCT_PACKAGES += \
 
 # Encryption
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
 
 # FM Radio
 PRODUCT_PACKAGES += \
