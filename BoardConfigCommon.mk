14696d0d2e6eb143decdeea0baed724f36d50a09
Jarl-Penguin <jarlpenguin@outlook.com>
2020-01-07 19:28:48 +0300

    msm8937-common: Always enable DexPreopt
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -98,18 +98,12 @@ TARGET_USES_GRALLOC1 := true
 TARGET_USES_ION := true
 TARGET_USES_HWC2 := true
 
+# DexPreopt
+WITH_DEXPREOPT := true
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
-
-# Enable dexpreopt to speed boot time
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-    endif
-  endif
-endif
 
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
