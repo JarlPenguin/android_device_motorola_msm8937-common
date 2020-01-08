9f706e470395ee587907aaa12d51ccf5bb638ff7
Jarl-Penguin <jarlpenguin@outlook.com>
2020-01-08 15:43:31 +0300

    msm8937-common: config.fs: add root dirs
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- config.fs
+++ config.fs
@@ -79,8 +79,32 @@ user:  AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
+[firmware/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[firmware/image/*]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
 [vendor/firmware_mnt/image/*]
 mode: 0771
 user: AID_SYSTEM
 group: AID_SYSTEM
+caps: 0
+
+[persist/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[dsp/]
+mode: 0771
+user: AID_MEDIA
+group: AID_MEDIA
 caps: 0
