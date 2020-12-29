ff01966ed846b325f93bc02c240f057a71cd0332
karthick111 <karthickrko61@gmail.com>
2020-12-29 12:08:41 +0530

    msm8937-common: Update media stack to R
    Change-Id: I0ea5f1cb32b93971a7772b62e62190479fd94fd7

--- properties.mk
+++ properties.mk
@@ -133,11 +133,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.mm.enable.qcom_parser=4643 \
-    vendor.vidc.enc.narrow.searchrange=1 \
+    media.stagefright.audio.sink=280 \
+    media.stagefright.thumbnail.prefer_hw_codecs=true \
+    vendor.mm.enable.qcom_parser=4176895 \
+    vendor.vidc.dec.downscalar_width=1920 \
+    vendor.vidc.dec.downscalar_height=1088 \
+    vendor.vidc.enc.disable_bframes=1 \
+    vendor.vidc.enc.disable_pframes=1 \
     vendor.vidc.enc.disable.pq=true \
+    vendor.vidc.enc.narrow.searchrange=1 \
+    vendor.video.disable.ubwc=1 \
     vendor.vidc.disable.split.mode=1 \
-    vendor.video.disable.ubwc=1
+    vidc.enc.disable.pq=true
 
 # Memory optimizations
 PRODUCT_PROPERTY_OVERRIDES += \
