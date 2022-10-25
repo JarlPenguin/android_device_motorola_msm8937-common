dcdd0c638eeba206410229bbfa3947a9a42d52c8
Jarl-Penguin <jarlpenguin@outlook.com>
2022-10-25 11:46:35 -0700

    msm8937-common: Clean up properties
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I3c9d6ceacd42fd71f078cde4a41d58f0ace89ffb

--- properties.mk
+++ properties.mk
@@ -54,20 +54,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     camera.disable_zsl_mode=1 \
-    persist.vendor.camera.display.umax=1920x1080 \
-    persist.vendor.camera.display.lmax=1280x720 \
     persist.vendor.camera.HAL3.enabled=1 \
     persist.vendor.qti.telephony.vt_cam_interface=1
 
-# Charger
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.charger.enable_suspend=true
-
 # CNE
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.cnd.iwlan=1 \
-    persist.vendor.cne.logging.qxdm=3974
-
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.vendor.cne.feature=1
 
@@ -91,28 +81,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1
 
-# Display
+# FM
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.gralloc.enable_fb_ubwc=1 \
-    debug.sf.disable_backpressure=1 \
-    debug.sf.enable_hwc_vds=1 \
-    debug.sf.latch_unsignaled=1 \
-    ro.hardware.egl=adreno \
-    ro.hardware.vulkan=adreno \
-    ro.sf.hwc_set_default_colormode=true \
-    ro.vendor.display.cabl=2 \
-    vendor.display.disable_skip_validate=1 \
-    vendor.display.enable_default_color_mode=1 \
-    vendor.gralloc.disable_ahardware_buffer=1 \
-    vendor.gralloc.enable_fb_ubwc=1
-
-# Fingerprint
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.qfp=false
-
-# Fm
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.fm.transmitter=false \
     ro.vendor.fm.use_audio_session=true
 
 # FRP
@@ -123,18 +93,27 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.egl.hw=0 \
     debug.enable.sglscale=1 \
-    vendor.gralloc.enable_fb_ubwc=1 \
+    debug.gralloc.enable_fb_ubwc=1 \
+    debug.sf.disable_backpressure=1 \
+    debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=0 \
+    debug.sf.latch_unsignaled=1 \
     debug.sf.recomputecrop=0 \
     dev.pm.dyn_samplingrate=1 \
     persist.hwc.enable_vds=1 \
     persist.hwc.mdpcomp.enable=true \
     persist.sys.wfd.virtual=0 \
+    ro.hardware.egl=adreno \
+    ro.hardware.vulkan=adreno \
+    ro.sf.hwc_set_default_colormode=true \
+    ro.vendor.display.cabl=2 \
     sdm.debug.disable_rotator_split=1 \
     sdm.debug.disable_skip_validate=1 \
     vendor.display.disable_rotator_downscale=1 \
     vendor.display.disable_skip_validate=1 \
-    sdm.perf_hint_window=50
+    vendor.display.enable_default_color_mode=1 \
+    vendor.gralloc.disable_ahardware_buffer=1 \
+    vendor.gralloc.enable_fb_ubwc=1
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -156,14 +135,14 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so \
     ro.vendor.qti.sys.fw.bg_apps_limit=60
 
-# QTI
+# Qualcomm
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.va_aosp.support=1
 
 PRODUCT_ODM_PROPERTIES += \
     ro.vendor.qti.va_odm.support=1
 
-# RIL
+# Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.ims.dropset_feature=0 \
     persist.vendor.ims.disableADBLogs=0 \
@@ -192,6 +171,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.procedure_bytes=SKIP \
     persist.vendor.radio.qcril_uim_vcc_feature=1 \
     persist.vendor.radio.relay_oprt_change=1 \
+    persist.vendor.radio.sar_sensor=1 \
     persist.vendor.radio.sw_mbn_update=1 \
     rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
     ro.build.vendorprefix=/vendor \
@@ -203,7 +183,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.radio.sar_sensor=1 \
     ro.vendor.sensors.pmd=false \
     ro.vendor.sensors.rmd=false \
     ro.vendor.sensors.smd=false \
@@ -228,17 +207,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.use_color_management=true
-
-# USB
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.usb.bpt=2ec1 \
-    ro.usb.bpt_adb=2ec5 \
-    ro.usb.bpteth=2ec3 \
-    ro.usb.bpteth_adb=2ec6 \
-    ro.usb.mtp=2e82 \
-    ro.usb.mtp_adb=2e76 \
-    ro.usb.ptp=2e83 \
-    ro.usb.ptp_adb=2e84
 
 # Wi-Fi
 PRODUCT_PROPERTY_OVERRIDES += \
